f2cd9e2db95f941f27225d2b41eb484b2cb155b0
HugoBoxFr <hugo.prea@sfr.fr>
2019-10-03 22:59:01 +0200

    map exo3

--- map/ex3.js
+++ map/ex3.js
@@ -59,8 +59,39 @@ En sortie:
 
  */
 
+const movies = [
+  {
+    name: 'Crazy Rich Asians',
+    rating: 93
+  },
+  {
+    name: 'Skyscraper',
+    rating: 46
+  },
+  {
+    name: 'Leave No Trace',
+    rating: 100
+  },
+  {
+    name: 'White Boy Rick',
+    rating: 60
+  }
+]
+
 function getMoviesFreshness(movies) {
-}
+  return movies.map(movie => {
+    if (movie.rating < 60) {
+      movie.label = 'rotten';
+    } else if (movie.rating >= 60 && movie.rating <= 75) {
+      movie.label = 'fresh';
+    } else {
+      movie.label = 'certified fresh'
+    };
+    return movie;
+  });
+};
+
+console.log(getMoviesFreshness(movies))
 
 
 
